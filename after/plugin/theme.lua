dec4c704418cb2712e39fd51b6fe517dc9b3d3b3
Ludvik Bjørklund <ludvikb@hotmail.no>
2024-02-06 08:23:26 +0100

    Set transparency in Telescope!

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -2,4 +2,17 @@ require("tokyonight").setup({
     style = "night",
     transparent = true
 })
+
+require("tokyonight").setup({
+    style = "night",
+    transparent = true,
+    on_highlights = function(hl, c)
+        hl.TelescopeNormal = {
+            fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+            fg = c.bg_dark,
+        }
+    end,
+})
 vim.cmd[[colorscheme tokyonight-night]]
