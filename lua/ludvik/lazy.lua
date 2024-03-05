c336f1c7652a10b4c5867402785636ae6faae3e8
Ludvik Bjørklund <ludvikb@hotmail.no>
2024-03-05 20:38:51 +0100

    Add harpoon

--- lua/ludvik/lazy.lua
+++ lua/ludvik/lazy.lua
@@ -36,9 +36,14 @@ local plugins = {
     {'folke/tokyonight.nvim', lazy = true, priority = 1000  },
     -- telescope
     {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
     }
 }
 
