d45fb1c70aa8e51468d794b20d98c1534f855f27
Ludvik Bjørklund <ludvikb@hotmail.no>
2024-03-07 08:29:47 +0100

    WIP: Add keymap to pop Harpoon's last added file

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -15,6 +15,9 @@ vim.keymap.set("n", "<C-n>", function() harpoon:list():select(4) end)
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
+-- Pop last added Harpoon file 
+vim.keymap.set("n", "<C-S-BS>", function() 
+end)
 
 -- basic telescope configuration
 local conf = require("telescope.config").values
