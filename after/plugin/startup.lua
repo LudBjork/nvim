3799514db708a28a8865bcc5b462f18033f0d959
LudBjork <ludvikb@hotmail.no>
2024-04-26 17:52:09 +0200

    Add gigachad startup :D

--- after/plugin/startup.lua
+++ after/plugin/startup.lua
@@ -1,1 +1,82 @@
-require("startup").setup({ theme = "evil" })
+local gigachad_header = {
+	[[  ⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⠛⠋⠉⠈⠉⠉⠉⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⢏⣴⣿⣷⠀⠀⠀⠀⠀⢾⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿  ]],
+	[[  ⣿⣿⣟⣾⣿⡟⠁⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣷⢢⠀  ⠀⠀⠀⠀⠀⢸  ]],
+	[[  ⣿⣿⣿⣿⣟⠀⡴⠄⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⣿  ]],
+	[[  ⣿⣿⣿⠟⠻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠶⢴⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⣿  ]],
+	[[  ⣿⣁⡀⠀⠀⢰⢠⣦⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⡄⠀⣴⣶⣿⡄⣿  ]],
+	[[  ⣿⡋⠀⠀⠀⠎⢸⣿⡆⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠗⢘⣿⣟⠛⠿⣼  ]],
+	[[  ⣿⣿⠋⢀⡌⢰⣿⡿⢿⡀⠀⠀⠀⠀⠀⠙⠿⣿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣧⢀⣼  ]],
+	[[  ⣿⣿⣷⢻⠄⠘⠛⠋⠛⠃⠀⠀⠀⠀⠀⢿⣧⠈⠉⠙⠛⠋⠀⠀⠀⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣧⠀⠈⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠀⠀⠀⠀⢀⢃⠀⠀⢸⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⡿⠀⠴⢗⣠⣤⣴⡶⠶⠖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡸⠀⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⡀⢠⣾⣿⠏⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠉⠀⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣧⠈⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⡄⠈⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⣿⣦⣄⣀⣀⣀⣀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠙⣿⣿⡟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠁⠀⠀⠹⣿⠃⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿  ]],
+	[[  ⣿⣿⣿⣿⠿⠛⠉⠉⠁⠀⢻⣿⡇⠀⠀⠀⠀⠀⠀⢀⠈⣿⣿⡿⠉⠛⠛⠛⠉⠉  ]],
+	[[  ⣿⡿⠋⠁⠀⠀⢀⣀⣠⡴⣸⣿⣇⡄⠀⠀⠀⠀⢀⡿⠄⠙⠛⠀⣀⣠⣤⣤⠄⠀  ]],
+}
+
+local settings = {
+	header = {
+		type = "text",
+		align = "center",
+		fold_section = false,
+		title = "Header",
+		margin = 5,
+		content = gigachad_header,
+		highlight = "Statement",
+		default_color = "#000000",
+		oldfiles_amount = 0,
+	},
+	body = {
+		type = "mapping",
+		oldfiles_directory = false,
+		align = "center",
+		margin = 5,
+		highlight = "String",
+		default_color = "#039dfc",
+		content = {
+			{ " (( Find File ))", "Telescope find_files", "<leader>ff" },
+			{ " (( Find Word ))", "Telescope live_grep", "<leader>fg" },
+			{ " (( Recent Files ))", "Telescope oldfiles", "<leader>of" },
+			{ " (( Git Files ))", "Telescope git_files", "<leader>gf" },
+			{ " (( Git Status ))", "Telescope git_status", "<leader>gs" },
+		},
+	},
+	clock = {
+		type = "text",
+		content = function()
+			local clock = " " .. os.date("%H:%M")
+			local date = " " .. os.date("%d-%m-%y")
+			return { clock, date }
+		end,
+		oldfiles_directory = false,
+		align = "center",
+		fold_section = false,
+		title = "Clock",
+		margin = 5,
+		highlight = "TSString",
+		default_color = "#FFFFFF",
+		oldfiles_amount = 10,
+	},
+	colors = {
+		background = "#1f2227",
+		folded_section = "#56b6c2",
+	},
+
+	parts = {
+		"header",
+		"body",
+		"clock",
+	},
+}
+require("startup").setup(settings)
