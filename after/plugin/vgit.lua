40496b38602ba75023fe13bbda06cc8542a2a020
LudBjork <ludvikb@hotmail.no>
2024-05-02 08:41:42 +0200

    Change git diff shortcut to gd and add blame shortcut

--- after/plugin/vgit.lua
+++ after/plugin/vgit.lua
@@ -1,4 +1,5 @@
 local vgit = require("vgit")
 vgit.setup()
 
-vim.keymap.set("n", "<leader>D", vgit.buffer_diff_preview)
+vim.keymap.set("n", "<leader>gd", vgit.buffer_diff_preview)
+vim.keymap.set("n", "<leader>gb", vgit.buffer_gutter_blame_preview)
