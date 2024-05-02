b31a88838f41abf5cdd73002673a03bae29df020
LudBjork <ludvikb@hotmail.no>
2024-05-02 11:01:21 +0200

    Add buffer history keybind

--- after/plugin/vgit.lua
+++ after/plugin/vgit.lua
@@ -3,3 +3,4 @@ vgit.setup()
 
 vim.keymap.set("n", "<leader>gd", vgit.buffer_diff_preview)
 vim.keymap.set("n", "<leader>gb", vgit.buffer_gutter_blame_preview)
+vim.keymap.set("n", "<leader>gh", vgit.buffer_history_preview)
