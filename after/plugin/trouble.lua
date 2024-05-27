de502f71b0b2ee504e2729073a657476160b189a
LudBjork <ludvikb@hotmail.no>
2024-05-27 12:21:41 +0200

    Remove unused keymaps + replace telescope with trouble for troubleshooting

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,3 +1,11 @@
+vim.keymap.set("n", "<leader>tt", function()
+	require("trouble").toggle()
+end)
+
 vim.keymap.set("n", "gr", function()
 	require("trouble").toggle("lsp_references")
+end)
+
+vim.keymap.set("n", "<leader>fe", function()
+	require("trouble").toggle("workspace_diagnostics")
 end)
