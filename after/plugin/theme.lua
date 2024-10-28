e63a0d4742a47d918806e291ca6ecafcaa792504
LudBjork <ludvikb@hotmail.no>
2024-10-28 08:57:47 +0100

    Set some config settings for gruvbox-material

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,2 +1,8 @@
+-- this is a comment
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_foreground = "material"
+vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_enable_italic = 1
+
+-- Set all config before this line
 vim.cmd.colorscheme("gruvbox-material")
