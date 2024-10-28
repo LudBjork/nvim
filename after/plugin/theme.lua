1ab0d0dfb355d036fa00528dff882fd91cc879c7
LudBjork <ludvikb@hotmail.no>
2024-10-28 11:04:04 +0100

    Try gruvbox-material mix variant

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,8 +1,9 @@
 -- this is a comment
 vim.g.gruvbox_material_background = "hard"
-vim.g.gruvbox_material_foreground = "material"
+vim.g.gruvbox_material_foreground = "mix"
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_disable_italic_comment = 0
 
 -- Set all config before this line
 vim.cmd.colorscheme("gruvbox-material")
