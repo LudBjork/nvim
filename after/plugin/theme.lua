d72f9e95ce531b2daaa8ea15e9bf4b7f0ffe394c
LudBjork <ludvikb@hotmail.no>
2024-10-30 09:07:51 +0100

    Set gruvbox foreground and background to default

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,6 +1,6 @@
--- this is a comment
-vim.g.gruvbox_material_background = "hard"
-vim.g.gruvbox_material_foreground = "mix"
+vim.g.gruvbox_material_background = "medium"
+vim.g.gruvbox_material_foreground = "material"
+
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_disable_italic_comment = 0
