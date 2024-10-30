b17b062af45f04d9363168f5ca95b5d8358afb31
LudBjork <ludvikb@hotmail.no>
2024-10-30 12:46:10 +0100

    Dim inactive windows (gruvbox-material config)

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -2,8 +2,8 @@ vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_foreground = "mix"
 
 vim.g.gruvbox_material_enable_bold = 1
-vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_disable_italic_comment = 0
+vim.g.gruvbox_material_dim_inactive_windows = 1
 
 -- Set all config before this line
 vim.cmd.colorscheme("gruvbox-material")
