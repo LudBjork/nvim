07fd90414ae68ba7da69d951a746ab8157765f48
LudBjork <ludvikb@hotmail.no>
2024-10-30 12:39:39 +0100

    Change bg and fg style

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,5 +1,5 @@
-vim.g.gruvbox_material_background = "medium"
-vim.g.gruvbox_material_foreground = "material"
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_foreground = "mix"
 
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_enable_italic = 1
