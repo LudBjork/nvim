b75d835d2876bece427abe80fb2cc1f7d7b665aa
LudBjork <ludvikb@hotmail.no>
2025-08-05 14:08:32 +0200

    Remove '-lock.json' files from telescope.
    Includes file search and live-grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,3 +6,11 @@ vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find, {})
+
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = {
+			".+%-lock.json", -- % to treat - as "-"
+		},
+	},
+})
